17dd31d9b0cfd177e81f82219ecc781299a79843
James Price <james.price@zincoverde.com>
2026-08-07 22:12:21 -0400

    Advance enforced Gather cap

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Keep StableDiffusion on the same kernel-aware MLX revision as the app.
         .package(
             url: "https://github.com/JamesPriceZV/mlx-swift",
-            revision: "bf141d9"
+            revision: "8d5be31"
         ),
 
         // Note: used by StableDiffusion library to download weights
