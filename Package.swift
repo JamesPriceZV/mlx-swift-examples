9d6a09165a5b0373c403ee5041b4fa4dffbf2b19
James Price <james.price@zincoverde.com>
2026-08-07 22:07:58 -0400

    Advance corrected MLX block cap

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Keep StableDiffusion on the same kernel-aware MLX revision as the app.
         .package(
             url: "https://github.com/JamesPriceZV/mlx-swift",
-            revision: "95c48ee"
+            revision: "bf141d9"
         ),
 
         // Note: used by StableDiffusion library to download weights
